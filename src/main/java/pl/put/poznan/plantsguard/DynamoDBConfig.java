2466e07c16b7e873e6412e558ef5bdb049561fca
kamil.kolodziejski <kamil.kolodziejski>
2017-10-26 00:50:16 +0200

    xray and config

--- src/main/java/pl/put/poznan/plantsguard/DynamoDBConfig.java
+++ src/main/java/pl/put/poznan/plantsguard/DynamoDBConfig.java
@@ -19,16 +19,11 @@ public class DynamoDBConfig {
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
  
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
- 
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
  
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         AmazonDynamoDB amazonDynamoDB 
-          = new AmazonDynamoDBClient(amazonAWSCredentials());
+          = new AmazonDynamoDBClient(); //amazonAWSCredentials());
          
         if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
             amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
@@ -37,10 +32,10 @@ public class DynamoDBConfig {
         return amazonDynamoDB;
     }
  
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(
-          amazonAWSAccessKey, amazonAWSSecretKey);
-    }
+//    @Bean
+//    public AWSCredentials amazonAWSCredentials() {
+//        return new BasicAWSCredentials(
+//          amazonAWSAccessKey, amazonAWSSecretKey);
+//    }
 
 }
