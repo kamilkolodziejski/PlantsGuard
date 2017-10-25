e1d7faebea1e82f4563f35ef1b31d0a2ae09baff
kamil.kolodziejski <kamil.kolodziejski>
2017-10-26 00:09:32 +0200

    update

---
+++ src/main/java/pl/put/poznan/plantsguard/service/MeasureService.java
@@ -0,0 +1,18 @@
+package pl.put.poznan.plantsguard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.put.poznan.plantsguard.model.Measure;
+
+@Service
+public class MeasureService {
+	
+	@Autowired
+	MeasuresRepository repository;
+	
+	public void save(Measure measure) {
+		repository.save(measure);
+	}
+
+}
