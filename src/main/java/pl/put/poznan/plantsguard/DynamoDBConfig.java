ee13fb2163ecdcbd092cb1dae620e71fe4481168
kamil.kolodziejski <kamil.kolodziejski>
2017-10-27 02:41:49 +0200

    update

--- src/main/java/pl/put/poznan/plantsguard/DynamoDBConfig.java
+++ src/main/java/pl/put/poznan/plantsguard/DynamoDBConfig.java
@@ -1,41 +1,49 @@
-//package pl.put.poznan.plantsguard;
-//
-//import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.util.StringUtils;
-//
-//import com.amazonaws.auth.AWSCredentials;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-//import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-//
-//@Configuration
-//@EnableDynamoDBRepositories
-//  (basePackages = "pl.put.poznan.plantsguard.service")
-//public class DynamoDBConfig { 
-//	
-//    @Value("${amazon.dynamodb.endpoint}")
-//    private String amazonDynamoDBEndpoint;
-// 
-// 
+package pl.put.poznan.plantsguard;
+
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+@Configuration
+@EnableDynamoDBRepositories
+  (basePackages = "pl.put.poznan.plantsguard.service")
+public class DynamoDBConfig { 
+	
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+    
+    @Value("${amazon.dynamodb.region}")
+    private String amazonDynamoDBRegion;
+ 
+ 
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB 
+          = new AmazonDynamoDBClient(); //amazonAWSCredentials());
+         
+        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        }
+        if(!StringUtils.isEmpty(amazonDynamoDBRegion)) {
+        	amazonDynamoDB.setRegion(Region.getRegion(Regions.fromName(amazonDynamoDBRegion)));
+        }
+         
+        return amazonDynamoDB;
+    }
+ 
 //    @Bean
-//    public AmazonDynamoDB amazonDynamoDB() {
-//        AmazonDynamoDB amazonDynamoDB 
-//          = new AmazonDynamoDBClient(); //amazonAWSCredentials());
-//         
-//        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-//            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
-//        }
-//         
-//        return amazonDynamoDB;
+//    public AWSCredentials amazonAWSCredentials() {
+//        return new BasicAWSCredentials(
+//          amazonAWSAccessKey, amazonAWSSecretKey);
 //    }
-// 
-////    @Bean
-////    public AWSCredentials amazonAWSCredentials() {
-////        return new BasicAWSCredentials(
-////          amazonAWSAccessKey, amazonAWSSecretKey);
-////    }
-//
-//}
+
+}
