d98e988bbc2d29e40e7c8f98d8ab7d76b8f9c6cf
kamil.kolodziejski <kamil.kolodziejski>
2017-10-30 02:23:38 +0100

    update

--- src/main/java/pl/put/poznan/plantsguard/service/UserAuthenticationProvider.java
+++ src/main/java/pl/put/poznan/plantsguard/service/UserAuthenticationProvider.java
@@ -1,17 +1,17 @@
-//package pl.put.poznan.plantsguard.service;
-//
-//import java.util.ArrayList;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//
-//@Component
-//public class UserAuthenticationProvider implements AuthenticationProvider {
+package pl.put.poznan.plantsguard.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class UserAuthenticationProvider{// implements AuthenticationProvider {
 //
 //	@Autowired
 //	private UserService userService;
@@ -37,5 +37,5 @@
 //		// TODO Auto-generated method stub
 //		return false;
 //	}
-//
-//}
+
+}
