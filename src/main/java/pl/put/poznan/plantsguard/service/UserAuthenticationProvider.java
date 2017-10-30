51fe5f21bb7b8e7cc6b6d1f58fc1d998fc3c91b6
kamil.kolodziejski <kamil.kolodziejski>
2017-10-30 02:18:28 +0100

    update

--- src/main/java/pl/put/poznan/plantsguard/service/UserAuthenticationProvider.java
+++ src/main/java/pl/put/poznan/plantsguard/service/UserAuthenticationProvider.java
@@ -1,41 +1,41 @@
-package pl.put.poznan.plantsguard.service;
-
-import java.util.ArrayList;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Component
-public class UserAuthenticationProvider implements AuthenticationProvider {
-
-	@Autowired
-	private UserService userService;
-	
-	@Override
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String name = authentication.getName();
-        String password = authentication.getCredentials().toString();
-         
-        if (userService.authenticate(name, password)) {
-  
-            // use the credentials
-            // and authenticate against the third-party system
-            return new UsernamePasswordAuthenticationToken(
-              name, password, new ArrayList<>());
-        } else {
-            return null;
-        }
-	}
-
-	@Override
-	public boolean supports(Class<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-}
+//package pl.put.poznan.plantsguard.service;
+//
+//import java.util.ArrayList;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//@Component
+//public class UserAuthenticationProvider implements AuthenticationProvider {
+//
+//	@Autowired
+//	private UserService userService;
+//	
+//	@Override
+//	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        String name = authentication.getName();
+//        String password = authentication.getCredentials().toString();
+//         
+//        if (userService.authenticate(name, password)) {
+//  
+//            // use the credentials
+//            // and authenticate against the third-party system
+//            return new UsernamePasswordAuthenticationToken(
+//              name, password, new ArrayList<>());
+//        } else {
+//            return null;
+//        }
+//	}
+//
+//	@Override
+//	public boolean supports(Class<?> arg0) {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//}
